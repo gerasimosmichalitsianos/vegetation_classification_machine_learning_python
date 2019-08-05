fe6a44461ce61a752960af40da56a1612c70ce05
Gerasimos Michalitsianos <gerasimosmichalitsianos@gmail.com>
2019-08-05 14:49:34 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='VegetationClassification',
+    version='1.0.0',
+    scripts=['bin/VegetationClassification.py','bin/ImageClassification.py','bin/Misc.py','bin/TrainingPoints.py','bin/TrainingImagery.py',], 
+    license='MIT',
+    include_package_data=True, 
+    long_description=open('README.txt').read(),
+    entry_points = {
+        'console_scripts': ['VegetationClassification=VegetatationClassification:main'],
+    }
+	
+)
